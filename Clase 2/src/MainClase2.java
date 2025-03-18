ccf6609ab60bea2dee32391a64d95dd56e6cb128
nataliacm93 <nataliadam@outlook.es>
2025-03-18 19:46:27 +0100

    Clases Programación
    Signed-off-by: nataliacm93 <nataliadam@outlook.es>

---
+++ Clase 2/src/MainClase2.java
@@ -0,0 +1,11 @@
+public class MainClase2 {
+    public static void main(String[] args) {
+        //Operador relacionales
+        int x = 10;
+        int y = 20;
+        System.out.println("¿Es x igual a y?" + (x == y));
+        System.out.println("¿Es x diferente a y?:" + (x != y));
+        System.out.println("¿Es x mayor que y?: " + (x>y));
+
+    }
+}
